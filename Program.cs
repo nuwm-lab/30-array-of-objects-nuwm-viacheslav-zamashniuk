92ae52525136507d0cf162d0efc9717054edd8ec
Viacheslav Zamashniuk KN-22 <zamashniuk_ak24@nuwm.edu.ua>
2025-10-04 14:21:36 +0300

    Task Complete

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Numerics;
 
 namespace LabWork
 {
@@ -9,16 +10,89 @@ namespace LabWork
     // за посиланням https://www.youtube.com/@ViktorZhukovskyy/videos 
 
     class Result
-    { 
-    // TODO: do it !
+    {
+        private double x;
+        private double y;
+        private double z;
+
+        // Конструктор
+        public Result(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        // Метод для введення координат
+        public void InputCoordinates(double vx, double vy, double vz)
+        {
+            x = vx;
+            y = vy;
+            z = vz;
+        }
+
+        // Обчислення довжини вектора
+        public double GetLength()
+        {
+            return Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2) + Math.Pow(z, 2));
+        }
+
+        // Вивід координат
+        public void Print()
+        {
+            Console.WriteLine($"({x}, {y}, {z})");
+        }
+
+        // Деструктор
+        ~Result()
+        {
+            Console.WriteLine("Об'єкт Vector знищено (Garbage Collector)");
+        }
     }
-    
+
     class Program
     {
         static void Main(string[] args)
         {
-            
-            Console.WriteLine("Hello World!");
+            Console.Write("Введіть кількість векторів: ");
+            int count = Convert.ToInt32(Console.ReadLine());
+
+            Result[] vectors = new Result[count];
+
+            // Заповнення масиву
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine($"Вектор {i + 1}:");
+                Console.Write("x = ");
+                double x = Convert.ToDouble(Console.ReadLine());
+                Console.Write("y = ");
+                double y = Convert.ToDouble(Console.ReadLine());
+                Console.Write("z = ");
+                double z = Convert.ToDouble(Console.ReadLine());
+
+                vectors[i] = new Result(x, y, z);
+            }
+
+            // Пошук вектора з максимальною довжиною
+            double maxLength = vectors[0].GetLength();
+            int maxIndex = 0;
+
+            for (int i = 1; i < count; i++)
+            {
+                if (vectors[i].GetLength() > maxLength)
+                {
+                    maxLength = vectors[i].GetLength();
+                    maxIndex = i;
+                }
+            }
+
+            Console.WriteLine($"\nВектор №{maxIndex + 1} має найбільшу довжину:");
+            vectors[maxIndex].Print();
+            Console.WriteLine($"Довжина = {maxLength}");
+
+            // Виклик GC (для демонстрації деструктора)
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
         }
     }
 }
